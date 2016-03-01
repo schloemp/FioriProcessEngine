48f26450a4ad18c071cd6a30ab5d559c63f3f619
AndrÃ© Schloemp <andre.schloemp@prologa.de>
2016-03-01 16:30:23 +0000

    Configuration hinzu

--- webapp/Component.js
+++ webapp/Component.js
@@ -21,13 +21,36 @@ sap.ui.define([
 		init: function() {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
-
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
 			// set the config model
 			var oConfigModel = models.createConfigModel(this.getMetadata());
 			this.setModel(oConfigModel, "config");
-			oConfigModel.attachRequestCompleted(function() { InitPE.init(oConfigModel); }, InitPE);
+			oConfigModel.attachRequestCompleted(function() {
+				InitPE.init(oConfigModel);
+			}, InitPE);
+			this.getRouter().initialize();
+		    this.getRouter().attachRouteMatched(this.onRouteMatched, this);
+		},
+		onRouteMatched: function(aEvent) {
+			var lConfig = aEvent.getParameter("config");
+			var lTarget = aEvent.getParameter("targetControl");
+			var lView = aEvent.getParameter("view");
+
+			if (lView) {
+				if (lTarget instanceof sap.m.NavContainer && lConfig.targetAggregation === "pages") {
+					lTarget.to(lView.getId());
+				} else {
+					if (lTarget instanceof sap.m.SplitContainer) {
+						if (lConfig.targetAggregation === "masterPages") {
+							lTarget.toMaster(lView.getId());
+						} else {
+							jQuery.sap.log.info("show detail");
+							lTarget.toDetail(lView.getId());
+						}
+					}
+				}
+			}
 		}
 	});
 
